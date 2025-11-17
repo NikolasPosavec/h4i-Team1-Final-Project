6a4b0b808203382dc2211e96f3036e7443408119
Alisha <alishawu2024@gmail.com>
2025-11-17 18:46:40 -0500

    basic setup

--- car-dealership/src/Login.jsx
+++ car-dealership/src/Login.jsx
@@ -1,6 +1,14 @@
 
-function Logout(){
+function Login(){
 
+    return(
+        <>
+        <div> 
+            <h1> Login Page: In Progress</h1> 
+            <button>Login</button> 
+        </div> 
+        </>
+    )
 }
 
 export default Logout
