62d7ca3309861a7c17ee0693f9277ee74dd533bd
NikolasPosavec <nikolas.posavec@gmail.com>
2025-11-18 10:01:53 -0500

    router cleaned up

--- car-dealership/src/App.jsx
+++ car-dealership/src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react';
 
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import Landing from "./pages/Landing";
