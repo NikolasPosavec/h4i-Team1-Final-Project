40bf627d6e2ee66d83c7ed2ad450e4219de83d9d
Victoria Xiao <vxiao@terpmail.umd.edu>
2025-11-19 12:43:23 -0500

    user auth and login page

--- car-dealership1/src/App.tsx
+++ car-dealership1/src/App.tsx
@@ -1,19 +1,46 @@
-// import { useState } from 'react';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { auth } from "./firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+import type { User } from "firebase/auth";
+import { useState, useEffect } from "react";
 import Login from "../../car-dealership1/src/pages/Login";
 import Cart from "../../car-dealership1/src/pages/Cart";
 import Landing from "../../car-dealership1/src/pages/Landing";
 import Contact from "../../car-dealership1/src/pages/Contact";
+import ForgotPassword from "../../car-dealership1/src/pages/ForgotPassword";
+
 
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsub;
+  }, []);
+
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path = "/" element = {<Landing />}/>
+      <Routes>
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" replace /> : <Login />}
+        />
+        <Route
+          path="/forgot-password"
+          element={user ? <Navigate to="/" replace /> : <ForgotPassword />}
+        />
+       
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route 
+        path="/" 
+        element={user ? <Landing /> : <Navigate to="/login" replace />}
+      />
       <Route path = "/cart" element = {<Cart />}/>
       <Route path = "/contact" element = {<Contact />}/>
       <Route path = "/login" element = {<Login />} />
