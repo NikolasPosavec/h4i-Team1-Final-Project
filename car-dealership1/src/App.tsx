3d2d18e7cbe1b4a025409f587dc716a02e4e73e1
vishal <vbalagani8@gmail.com>
2025-11-20 17:35:45 -0500

    deleted duplicate routes

--- car-dealership1/src/App.tsx
+++ car-dealership1/src/App.tsx
@@ -1,17 +1,13 @@
-
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { auth } from "./firebase/config";
 import { onAuthStateChanged } from "firebase/auth";
 import type { User } from "firebase/auth";
 import { useState, useEffect } from "react";
-import Login from "../../car-dealership1/src/pages/Login";
-import Cart from "../../car-dealership1/src/pages/Cart";
-import Landing from "../../car-dealership1/src/pages/Landing";
-import Contact from "../../car-dealership1/src/pages/Contact";
-import ForgotPassword from "../../car-dealership1/src/pages/ForgotPassword";
-
-
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Landing from "./pages/Landing";
+import Contact from "./pages/Contact";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
@@ -21,7 +17,6 @@ function App() {
     });
     return unsub;
   }, []);
-
 
   return (
     <BrowserRouter>
@@ -34,18 +29,15 @@ function App() {
           path="/forgot-password"
           element={user ? <Navigate to="/" replace /> : <ForgotPassword />}
         />
-       
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route 
-        path="/" 
-        element={user ? <Landing /> : <Navigate to="/login" replace />}
-      />
-      <Route path = "/cart" element = {<Cart />}/>
-      <Route path = "/contact" element = {<Contact />}/>
-      <Route path = "/login" element = {<Login />} />
-    </Routes>
+
+        <Route
+          path="/"
+          element={user ? <Landing /> : <Navigate to="/login" replace />}
+        />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-export default App
+export default App;
