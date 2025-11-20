01068b49bead072b7e3e7726fd53b3ade91dd05c
vishal <vbalagani8@gmail.com>
2025-11-20 17:52:02 -0500

    manually adding changes

--- car-dealership1/src/App.tsx
+++ car-dealership1/src/App.tsx
@@ -1,24 +1,43 @@
-// import { useState } from 'react';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "../../car-dealership1/src/pages/Login";
-import Cart from "../../car-dealership1/src/pages/Cart";
-import Landing from "../../car-dealership1/src/pages/Landing";
-import Contact from "../../car-dealership1/src/pages/Contact";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { auth } from "./firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+import type { User } from "firebase/auth";
+import { useState, useEffect } from "react";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Landing from "./pages/Landing";
+import Contact from "./pages/Contact";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsub;
+  }, []);
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path = "/" element = {<Landing />}/>
-      <Route path = "/cart" element = {<Cart />}/>
-      <Route path = "/contact" element = {<Contact />}/>
-      <Route path = "/login" element = {<Login />} />
-    </Routes>
+      <Routes>
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" replace /> : <Login />}
+        />
+        <Route
+          path="/forgot-password"
+          element={user ? <Navigate to="/" replace /> : <ForgotPassword />}
+        />
+
+        <Route
+          path="/"
+          element={user ? <Landing /> : <Navigate to="/login" replace />}
+        />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-export default App
+export default App;
