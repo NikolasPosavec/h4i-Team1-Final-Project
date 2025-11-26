d313837722f0c34efae014dd257ab32d20f2a9f2
vishal <vbalagani8@gmail.com>
2025-11-26 01:20:39 -0500

    persistent logout

--- car-dealership1/src/pages/Contact.tsx
+++ car-dealership1/src/pages/Contact.tsx
@@ -1,23 +1,14 @@
-import { signOut } from "firebase/auth";
-import { auth } from "../firebase/config";
-import { Navbar } from "../components/navbar";
+import Navbar from "../components/navbar";
 
-function Contact(){
-const handleLogout = async () => {
-    try {
-      await signOut(auth);
-    } catch (err) {
-      console.error("Logout error:", err);
-    }
-  };
-    return(
-        <>
-        <div> 
-            <h1> Contact Page: In Progress</h1> 
-            <Navbar onLogout={handleLogout}/>
-        </div> 
-        </>
-    )
+function Contact() {
+  return (
+    <>
+      <div>
+        <h1> Contact Page: In Progress</h1>
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
